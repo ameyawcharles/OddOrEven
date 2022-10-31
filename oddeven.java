5ac2d6599c618690b0c79f505e0784f5eda32493
ameyawcharles <icharlesame@gmail.com>
2022-10-31 22:39:19 +0000

    First Commit

---
+++ oddeven.java
@@ -0,0 +1,27 @@
+// Find Odd or Even 
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+public static void main(String[] args) {
+	
+	int x;
+
+	System.out.println("Enter an integer to check if it's odd or even");
+	Scanner in = new Scanner(System.in);
+
+	x = in.nextln();
+
+	if(x % 2 == 0)
+
+	System.out.println("The number is even");
+
+else 
+
+	system.out.print("The number is odd");
+
+
+    }  
+
+}
